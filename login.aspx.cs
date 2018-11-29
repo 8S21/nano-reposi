25632fff9b620b10198aebd5a01a2867eacac0f1
nano2509 <45247392+nano2509@users.noreply.github.com>
2018-11-29 10:06:12 -0600

    Create login.aspx.cs

---
+++ login.aspx.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Security;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Unnamed1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+           
+    }
+
+    protected void Unnamed1_LoggedIn(object sender, EventArgs e)
+    {
+
+    }
+}
